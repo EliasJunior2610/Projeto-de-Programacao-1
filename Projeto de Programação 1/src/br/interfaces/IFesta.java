de262489861ee7db9624b20c1ea87fe894a41a7c
EliasJunior2610 <e26junior2002@gmail.com>
2022-11-16 19:35:33 -0300

    Update

---
+++ Projeto de Programação 1/src/br/interfaces/IFesta.java
@@ -0,0 +1,6 @@
+package br.interfaces;
+
+public interface IFesta {
+    //abre uma classe boolean, já que, é possivel escolher apenas dois valores, se vai ou não ir pro camarote.
+    public boolean camarote = true;
+}
---
+++ Projeto de Programação 1/src/br/interfaces/IShow.java
@@ -0,0 +1,8 @@
+package br.interfaces;
+
+public interface IShow {
+    //abre uma classe string para representar o nome da banda.
+    public String nomeDaBanda = "Mamonas Assassinas";
+//abre uma classe boolean, já que, deve se escolher entre dois valores, nesse caso se vai ou não ir para o camarote.
+    public boolean camarote = true;
+}
